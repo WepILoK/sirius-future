f28834ea56db4be05b05ab1e9b603c3668a73016
WepILoK <artur.nasyrov1999@mail.ru>
2021-05-28 06:17:34 +0300

    Create

---
+++ pages/greatjob.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Layout} from "../components/Layout";
+
+export default function GreatJob () {
+    return (
+        <Layout>
+            Хорошая работа
+        </Layout>
+    );
+};
+
